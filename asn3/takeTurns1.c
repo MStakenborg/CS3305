3070346a3d4b98c4a0270098a1fb742cb43b4063
Michelle Stakenborg <mstakenb@uwo.ca>
2015-03-17 00:48:26 +0000

    Take Turns 1 Cleanup & Commented*/

--- asn3/takeTurns1.c
+++ asn3/takeTurns1.c
@@ -1,3 +1,14 @@
+/***********************************************************
+ * Assignment 3 Part 1 - Taking Turns 1                    *
+ * Michelle Stakenborg                                     *
+ * Course: CS3305                                          *
+ *                                                         *
+ * This program creates two threads A and B and            *
+ * utilizes semaphores to allow the threads to take       *
+ * turns executing a print statement                       *
+ *                                                         *
+ * ********************************************************/
+
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -5,13 +16,11 @@
 #include <semaphore.h>
 #define NUMTHREAD 2
 
-/*declare semaphore*/
+/*declare semaphore and global variable*/
 sem_t semA, semB;
-int counter; 
 int iters; 
-//int aTurn;
-//int bTurn; 
 
+/* operation A */
 void *printStmtA(void *arg)
 {
   int i=0; 
@@ -19,15 +28,18 @@ void *printStmtA(void *arg)
   {
       /*sem down*/
       sem_wait(&semB);
+
       /*CRITICAL SECTION*/
       char* thread = (char*)arg; 
       printf("Thread %s executing op%s\n", thread, thread);
       /*END CRITICAL SECTION*/
+      
       sem_post(&semA);
       i++;
   }
 }
 
+/* operation B */
 void *printStmtB(void *arg)
 {
   int i=0;
@@ -35,28 +47,35 @@ void *printStmtB(void *arg)
   {
     /*sem down*/
     sem_wait(&semA);
+
     /*CRITICAL SECTION*/
     char* thread = (char*)arg;
     printf("thread %s executing op%s\n", thread, thread);
     /*END CRITICAL SECTION*/
+    
     sem_post(&semB);
     i++;
   }
 }
 
+/* main program creates the threads and cleans up after return */
 int main(int argc, char** argv) 
 {
 
   /*pthreads A & B variables*/
   pthread_t threadA; 
   pthread_t threadB;
+
+  /*initialize semaphores*/
   sem_init(&semA, 0, 0); 
   sem_init(&semB, 0, 1);
 
+  /*variables to pass to print statement - Allows expansion*/
   char *charA = "A";
   char *charB = "B";
   int i; 
 
+  /*check for command line argument - assumes valid digit entered*/
   if (argc != 2){
       perror("Please enter number of iterations\n");
       return 1;
@@ -64,28 +83,26 @@ int main(int argc, char** argv)
 
   /*get number of iterations from command line argument*/
   iters = atoi(argv[1]);
-  counter = iters*NUMTHREAD; 
 
   /*create threads and execute print statements for number of iterations*/
- // for(i = 0; i < iters; i++)
- // {
-    if (pthread_create(&threadA, NULL, printStmtA, (void *)charA))
-    {
-      fprintf(stderr, "Error creating thread A\n");
-      exit(1);
-    }
+  if (pthread_create(&threadA, NULL, printStmtA, (void *)charA))
+  {
+    fprintf(stderr, "Error creating thread A\n");
+    exit(1);
+  }
 
-    if (pthread_create(&threadB, NULL, printStmtB, (void *)charB))
-    {
-      fprintf(stderr, "Error creating thread B\n");
-      exit(1);
-    }
-  //}
+  if (pthread_create(&threadB, NULL, printStmtB, (void *)charB))
+  {
+    fprintf(stderr, "Error creating thread B\n");
+    exit(1);
+  }
+ 
 
   /*join thread to avoid early exit and destroy sem after return*/
   pthread_join(threadA, NULL);
   pthread_join(threadB, NULL);
 
+  /*cleanup*/
   sem_destroy(&semA);
   sem_destroy(&semB);
   pthread_exit(0); 
